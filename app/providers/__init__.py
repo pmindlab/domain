cb12d5f5bb042246039b21530112a21b1e5e76a4
pmindlab <240002383+pmindlab@users.noreply.github.com>
2026-09-12 09:58:45 +0200

    Import NameLab v1.4 source

---
+++ app/providers/__init__.py
@@ -0,0 +1,5 @@
+from .gbif import GBIFProvider, TaxonHit
+from .domain import VerisignRdapProvider, DomainResult
+from .brand import BrandScreenProvider, BrandResult
+
+from .language import LanguageProvider, LanguageHit
